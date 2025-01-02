d21f5a7e5847b368b56926b2e00fb6f3477ba38c
Esteve <esteve.17mas@gmail.com>
2025-01-02 15:19:01 +0100

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -13,6 +17,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
