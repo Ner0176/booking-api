e14a102d661acd42f5e0873d9368ac0d0670ff51
Esteve <esteve.17mas@gmail.com>
2025-02-09 11:01:01 +0100

    refactor: just return userId after auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,10 @@
 import { Response } from 'express';
-import { CreateUserDto, UserDto } from '../user/dtos';
+import { CreateUserDto } from '../user/dtos';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dtos';
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import { Public } from 'src/decorators';
-import { Serialize, SetCookie } from 'src/interceptors';
+import { SetCookie } from 'src/interceptors';
 @Public()
 @Controller('auth')
 export class AuthController {
@@ -12,16 +12,16 @@ export class AuthController {
 
   @SetCookie()
   @Post('signup')
-  @Serialize(UserDto)
   async signUp(@Body() payload: CreateUserDto) {
-    return await this.authService.createUser(payload);
+    const user = await this.authService.createUser(payload);
+    return user.id;
   }
 
   @SetCookie()
   @Post('login')
-  @Serialize(UserDto)
   async login(@Body() payload: LoginUserDto) {
-    return await this.authService.validateUser(payload);
+    const user = await this.authService.validateUser(payload);
+    return user.id;
   }
 
   @Post('logout')
