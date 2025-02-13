dd5a54e53ed497519fb33361a1aab6e01f40b505
Esteve <esteve.17mas@gmail.com>
2025-02-13 18:44:34 +0100

    refactor: extract default_class_configs

--- src/class-configs/class-configs.controller.ts
+++ src/class-configs/class-configs.controller.ts
@@ -3,13 +3,20 @@ import { Admin } from 'src/decorators';
 import { ClassConfigsService } from './class-configs.service';
 import { UpdateClassConfigsDto } from './dtos';
 
+const DEFAULT_CLASS_CONFIGS = {
+  maxRecoveryDays: 60,
+  maxCancellationPerMonth: 2,
+  minHoursBeforeCancellation: 2,
+};
+
 @Controller('class-configs')
 export class ClassConfigsController {
   constructor(private classConfigService: ClassConfigsService) {}
 
   @Get()
   async getClassConfigs() {
-    return await this.classConfigService.getClassConfigs();
+    const classConfigs = await this.classConfigService.getClassConfigs();
+    return classConfigs.length > 0 ? classConfigs[0] : DEFAULT_CLASS_CONFIGS;
   }
 
   @Admin()
